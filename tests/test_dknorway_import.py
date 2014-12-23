8930cc896429b2ab1266ae9a224370c3765ea2ae
Erlend Dalen <erlend.dalen@gmail.com>
2014-12-23 10:19:22 +0100

    posten_postnrimport should not be counted for coverage.
    The file is never to be imported, only to be run as a task.

--- tests/test_dknorway_import.py
+++ tests/test_dknorway_import.py
@@ -2,7 +2,6 @@
 from unittest import TestCase
 import dknorway.models
 import dknorway.admin
-import dknorway.posten_postnrimport
 import dknorway.postnrcache
 
 class TestImport(TestCase):
@@ -10,5 +9,4 @@ class TestImport(TestCase):
     def test_dkmath_import(self):
         assert dknorway.models
         assert dknorway.admin
-        assert dknorway.posten_postnrimport
         assert dknorway.postnrcache
