da8aa0972b0e5c57ae21fc565914904e28088797
Bjorn Pettersen <bp@norsktest.no>
2019-09-14 17:05:36 +0200

    Remove test command from setup.py (it's never used, never tested)

--- setup.py
+++ setup.py
@@ -14,27 +14,11 @@ Topic :: Software Development :: Libraries
 """
 
 import setuptools
-from distutils.core import setup, Command
-
 
 version = '0.1.0'
 
 
-class PyTest(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
-
-
-setup(
+setuptools.setup(
     name='dknorway',
     version=version,
     url="https://github.com/datakortet/dknorway",
@@ -49,7 +33,6 @@ setup(
     description=__doc__.strip(),
     classifiers=[line for line in classifiers.split('\n') if line],
     long_description=open('README.rst').read(),
-    cmdclass={'test': PyTest},
-    packages=['dknorway'],
+    packages=setuptools.find_packages(exclude=['tests']),
     zip_safe=False,
 )
